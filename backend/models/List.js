449d870e57b19ea332821230d7802318f2c9f38d
ahmad_arafa25 <105568257+darkmaster02004@users.noreply.github.com>
2026-04-18 12:07:39 +0200

    discarded remaining changes from last commit

--- backend/models/List.js
+++ backend/models/List.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
 
-// a better approach is to make 2 separate relationship representations: user_list, and list_book
-//, user_list relationship can be represented by just adding userId in List attributes, refering a user
 const ListSchema = new mongoose.Schema({
-    // 'name' is more descriptive
     title: { type: String, required: true },
     userId: {
         type: mongoose.Schema.Types.ObjectId,
