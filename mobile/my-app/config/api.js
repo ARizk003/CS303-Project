70301ad00f08ebb3e9c320182e7c39e1e520aac6
hebamohameedd <heba29715@gmail.com>
2026-05-09 00:20:46 +0300

    fix list

--- mobile/my-app/config/api.js
+++ mobile/my-app/config/api.js
@@ -1,1 +1,6 @@
+<<<<<<< Updated upstream
 export const BASE_URL = 'http://192.168.1.8:5000';
+=======
+export const BASE_URL = 'http://192.168.1.5:5000';
+export const GROQ_API_KEY = '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';
+>>>>>>> Stashed changes
